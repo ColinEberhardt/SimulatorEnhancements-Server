f7d1005c93b9351c473006db62f0a70163238e4d
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2014-03-09 18:32:12 +0000

    Record and replay routes

--- map.js
+++ map.js
@@ -1,27 +1,62 @@
-var map;
 
-function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(viewModel.model.location.lat(),
-      viewModel.model.location.long()),
-    zoom: 10
+var mapOptions = {
+  center: new google.maps.LatLng(viewModel.model.location.lat(),
+    viewModel.model.location.long()),
+  disableDoubleClickZoom: true,
+  zoom: 10
+};
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+map.setOptions({draggableCursor:'crosshair'});
+
+var markersArray = [];
+
+google.maps.event.addListener(map, 'dblclick', function(event) {
+  viewModel.locations.push(event.latLng);
+});
+
+var pinImage = new google.maps.MarkerImage("https://chart.googleapis.com/chart?chst=d_map_spin&chld=0.4|0|FF0000|13|b|",
+    new google.maps.Size(17, 28),
+    new google.maps.Point(0,0),
+    new google.maps.Point(8, 28));
+
+viewModel.locations.subscribe(function(locations) {
+  clearOverlays();
+  for (var i = locations.length - 1; i >= 0; i--) {
+    var location = locations[i];
+    var marker  = new google.maps.Marker({
+      position: location,
+      icon: pinImage,
+      map: map
+    });
+    markersArray.push(marker);
   };
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  // To add the marker to the map, use the 'map' property
-  var marker = new google.maps.Marker({
-      position: mapOptions.center,
-      draggable: true
+  var path = new google.maps.Polyline({
+    path: locations,
+    geodesic: true,
+    strokeColor: '#FF0000',
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+    map: map
   });
+  markersArray.push(path);
+});
 
-  marker.setMap(map);
+var currentLocationMarker = undefined;
+viewModel.currentLocation.subscribe(function(currentLocation) {
+  if (!currentLocationMarker) {
+    currentLocationMarker = new google.maps.Marker({
+      position: currentLocation,
+      map: map
+    });
+  } else {
+    currentLocationMarker.setPosition(currentLocation);
+  }
+});
 
-  google.maps.event.addListener(marker, 'dragend', function() {
-    var long = marker.position.lng();
-    var lat = marker.position.lat();
-
-    viewModel.model.location.lat(lat);
-    viewModel.model.location.long(long);
-  });
+function clearOverlays() {
+  for (var i = 0; i < markersArray.length; i++ ) {
+    markersArray[i].setMap(null);
+  }
+  markersArray.length = 0;
 }
-google.maps.event.addDomListener(window, 'load', initialize);
--- viewModel.js
+++ viewModel.js
@@ -30,11 +30,40 @@ if (!ko.subscribable.fn.throttle) {
   };
 }
 
+
 function ViewModel() {
+  var that = this;
   this.model = new Model();
+
+  this.locations = ko.observableArray();
+
+  this.currentLocation = ko.observable();
+
+  this.play = function() {
+    var time = 0;
+    var handle = setInterval(function() {
+      var locationCount = that.locations().length - 1;
+      var index = Math.floor(locationCount * time);
+      var leftLocation = that.locations()[index];
+      var rightLocation = that.locations()[index + 1];
+      var fraction = (time * locationCount - index);
+
+      that.currentLocation(google.maps.geometry.spherical.interpolate(leftLocation, rightLocation, fraction));
+
+      time += 0.01;
+      if (time>=1.0) {
+        clearInterval(handle);
+      }
+    }, 100);
+  };
 
   this.serializedModel = ko.computed(function() {
     return ko.toJSON(this.model);
+  }, this);
+
+  this.currentLocation.subscribe(function(currentLocation) {
+    this.model.location.lat(currentLocation.lat());
+    this.model.location.long(currentLocation.lng());
   }, this);
 
   this.serializedModel.throttle(200, true).subscribe(function(newValue) {
